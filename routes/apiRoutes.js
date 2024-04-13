66260bc938f89dc2b6a4b17990ca6097d5ae78ca
gjudilla <gjudilla@gmail.com>
2024-04-12 23:29:27 -0700

    added basic routes and middleware

---
+++ routes/apiRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { readFromFile, writeToFile } = require('../helpers/fsUtils');
+const uuid = require('uuid');
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+// importing packages and routes
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
-
-const app = express(); 
+// setting up PORT 
 const PORT = process.env.PORT || 3001; 
+const app = express(); 
 
+
+// app.use((req,res,next) => {
+
+// })
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+// Server listening to client request and text is logged in console
 app.listen(PORT, () =>
   console.log(`Listening at http://localhost:${PORT}`)
 );
